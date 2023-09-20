1b9dff521e747ae7b5bf27c931f47a45e9f402eb
Chriiis1421 <106244596+Chriiis1421@users.noreply.github.com>
2023-09-20 15:09:35 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::redirect('/', '/cidades', 301);
-Route::get('/cidades', 'CidadeController');
+Route::resource('cidades', 'CidadeController');
